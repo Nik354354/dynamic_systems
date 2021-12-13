7042fa7870dd70378c5f188f80dcb2d0c3dea8e6
mehailov <mikhailov.siu@phystech.edu>
2021-12-14 01:58:32 +0300

    Окно подтверждения выхода и сохранения в xyz-файл

--- sitnikov_files.py
+++ sitnikov_files.py
@@ -1,17 +1,51 @@
 import json
-def fetch_xyz_data():
-	# return x1_seq, y1_seq, x2_seq, y2_seq, z_seq
-	pass
+import tkinter as tk
 
-def add_xyz_entry(coords, file_path='saves//sitnikov_save.xyz', comment=''):
-	'''Принимает координаты, название файла и комментарий, добавляет к концу файла новое положение системы'''
-	x1, y1, x2, y2, z = coords
-	'''with open(filename, 'a') as file:
-		3
-		comment
-		star x1 y1 0
-		star x2 y2 0
-		planet 0 0 z'''
+import my as my
+
+
+def fetch_xyz_data():
+	return x1_seq, y1_seq, x2_seq, y2_seq, z_seq
+
+
+class MessageBox(tk.Tk):
+	def __init__(self):
+		super().__init__()
+		save1_btn = tk.Label(self, text="Сохранить данные симуляции?")
+		save_btn= tk.Button(self, text="Сохранить", command=self.confirm)
+		dont_save_btn = tk.Button(self, text="Не сохранять", command=self.decline)
+		cancel_btn = tk.Button(self, text="Отмена", command=self.cancel)
+		save1_btn.pack()
+		save_btn.pack(side=tk.LEFT)
+		dont_save_btn.pack(side=tk.LEFT)
+		cancel_btn.pack(side=tk.LEFT)
+
+		proceed_to_exit = False
+
+	def confirm(self):
+		create_xyz_log()
+		proceed_to_exit = True
+		self.destroy()
+
+	def decline(self):
+		proceed_to_exit = True
+		self.destroy()
+
+	def cancel(self):
+		self.destroy()
+
+
+
+def add_xyz_entry(file_path='saves//sitnikov_save.xyz', comment=''):
+	'''Принимает название файла и комментарий, добавляет к концу файла новое положение системы'''
+	x1, y1, x2, y2, z =fetch_xyz_data()
+
+	with open(file_path, 'a') as file:
+		file.write("3")
+		file.write(comment)
+		file.write("star"+" "+x1+" "+y1+" 0")
+		file.write("star "+x2+" "+y2+" 0")
+		file.write("planet 0 0" + z)
 
 def create_xyz_log(filename='saves//sitnikov_save.xyz', comment=''):
 
@@ -25,3 +59,11 @@ def load_configuration(file='sitnikov_cfg.json'):
 	with open(file, 'r') as cfg:
 		config = json.load(cfg)
 	return config
+
+def ask_save():
+	'''Создаёт окно MessageBox, с подтверждением выхода без сохранения данных'''
+	confirm_exit = MessageBox()
+	confirm_exit.mainloop()
+	return confirm_exit
+
+
